b9a51b5fadd24b2849b96a96af80e42494db6db3
Xavier Ordoquy <xordoquy@linovia.com>
2014-04-01 21:45:43 +0200

    Get things ready for non postgres databases.

--- fab_bundle/bundle.py
+++ fab_bundle/bundle.py
@@ -25,28 +25,29 @@ def choose_postgres_template():
 
 def database_creation():
     bundle_name = env.http_host
-    installed_dbs = postgres('psql %s -l|grep UTF8')
-    installed_dbs = [db.split('|')[0].strip() for db in installed_dbs.split('\n')]
+    if 'postgres' in env:
+        installed_dbs = postgres('psql %s -l|grep UTF8')
+        installed_dbs = [db.split('|')[0].strip() for db in installed_dbs.split('\n')]
 
-    db_template = choose_postgres_template()
+        db_template = choose_postgres_template()
 
-    if env.databases:
-        for database in env.databases.values():
-            if database['NAME'] in installed_dbs:
-                continue
-            args = [
-                '%s',
-                '-T %s' % db_template,
-                '-E UTF8',
-            ]
-            if 'USER' in database:
-                args.append(' -O %s' % database['USER'])
-            args.append(database['NAME'])
-            postgres('createdb ' + ' '.join(args))
-    else:
-        if bundle_name not in installed_dbs:
-            postgres(('createdb %s -T %s '
-                 '-E UTF8 %s') % ('%s', db_template, bundle_name))
+        if env.databases:
+            for database in env.databases.values():
+                if database['NAME'] in installed_dbs:
+                    continue
+                args = [
+                    '%s',
+                    '-T %s' % db_template,
+                    '-E UTF8',
+                ]
+                if 'USER' in database:
+                    args.append(' -O %s' % database['USER'])
+                args.append(database['NAME'])
+                postgres('createdb ' + ' '.join(args))
+        else:
+            if bundle_name not in installed_dbs:
+                postgres(('createdb %s -T %s '
+                     '-E UTF8 %s') % ('%s', db_template, bundle_name))
 
 
 def database_migration():
